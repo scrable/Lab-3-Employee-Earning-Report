9688a24dafe46caa4052943b834c27a240b661fa
scrable <cruss.seq11@gmail.com>
2016-11-15 17:44:08 -0800

    Add a bunch of stuff Add summation and output

--- EmployeeEarnings.java
+++ EmployeeEarnings.java
@@ -4,7 +4,6 @@ public class EmployeeEarnings
 {
   public static void main(String[] args)
   {
-    boolean finished = false;
     EmployeeEarnings[] earnings;
     int employeeAmount = 0;
 
@@ -13,8 +12,9 @@ public class EmployeeEarnings
     System.out.print("Enter the number of employees: ");
     employeeAmount = in.nextInt();
     earnings = new EmployeeEarnings[employeeAmount];
+    int count = 0;
 
-    while (finished != true)
+    while (count < employeeAmount)
     {
 
       // create a report
@@ -25,6 +25,7 @@ public class EmployeeEarnings
       String name = in.next();
 
       // set employee name
+      // earnings[count].setEmployeeName(name);
       employee.setEmployeeName(name);
 
       // determine type of worker
@@ -37,44 +38,129 @@ public class EmployeeEarnings
         if (employeeType.equals("hourly"))
         {
           // get the hourly rate from user
+          System.out.print("Enter the hourly rate for " + name + ": ");
+          double rate = in.nextDouble();
+
           // set the hourly rate
+          employee.setHourlyRate(rate);
           valid = true;
+
+          // get hours worked from user
+          System.out.print("How many hours did " + name + " work? ");
+          double hours = in.nextDouble();
+
+          // set hours worked
+          employee.setHoursWorked(hours);
+
+          // calc the hourly workers pay
+          employee.calcTotalHourlyPay(hours, rate);
         }
-        else if (employeeType == "salary")
+        else if (employeeType.equals("salary"))
         {
           // get the salary rate from user
+          System.out.print("Enter the salary for " + name + ": ");
+          double rate = in.nextDouble();
+
           // set the salary rate
+          employee.setSalary(rate);
           valid = true;
         }
-        else if (employeeType == "commission")
+        else if (employeeType.equals("commission"))
         {
+          // get the sales
+          System.out.print("Enter the amount of weekly sales: ");
+          double sales = in.nextDouble();
+
+          // set the sales
+          employee.setCommission(sales);
           valid = true;
         }
         else
           System.out.println("Enter a valid input.");
       }
 
-      // get hours worked from user
-      // set hours worked
-
       // add the employee to the array
+      earnings[count] = employee;
 
-      // if no more employees, finished = true
+      // increment the count to next employee
+      count++;
     }
+    printEmployees(earnings, count);
+  }
+  public static double calcTotalPay(EmployeeEarnings[] earnings, int count)
+  {
+    // calc total pay from each worker in the array
+    double sum = 0;
+    for(int i = 0; i < count; i++)
+    {
+      sum += earnings[i].totalPay + earnings[i].salary;
+    }
+    return sum;
+  }
+  public static void printEmployees(EmployeeEarnings[] earnings, int count)
+  {
+    System.out.println(
+        "======================================================================================");
+    System.out.println(
+        "Name\t\tClass\t\tHours\tSales\t\tRate\tWeekly Pay Amount");
+    for (int i = 0; i < count; i++)
+    {
+      // if hourly worker
+      if (earnings[i].hourlyRate > 0)
+      {
+        System.out.println(earnings[i].employeeName + "\t\thourly\t\t" +
+                           earnings[i].hoursWorked + "\t\t\t" +
+                           earnings[i].hourlyRate + "\t" +
+                           earnings[i].totalPay);
+      }
+      // if salary worker
+      else if (earnings[i].salary > 0)
+      {
+        System.out.println(earnings[i].employeeName + "\t\tsalary\t\t\t\t\t\t" +
+                           earnings[i].salary);
+      }
+      // if commission worker
+      else if (earnings[i].commission > 0)
+      {
+        System.out.println(earnings[i].employeeName + "\t\tcommission\t\t" +
+                           (earnings[i].commission * 0.2));
+      }
+    }
+    System.out.println(
+        "======================================================================================");
+    System.out.println("\t\t\t\t\t\t\tTOTAL\t" + calcTotalPay(earnings, count));
   }
 
   // attributes
   double hoursWorked = 0.0;
   double hourlyRate = 0.0;
+  double totalPay = 0.0;
   double salary = 0.0;
+  double commission = 0.0;
   String employeeName = "";
 
   double getHourlyRate() { return hourlyRate; }
   double getSalary() { return salary; }
   String getEmployeeName() { return employeeName; }
   double getHoursWorked() { return hoursWorked; }
-  void setHourlylRate(double rate) { hourlyRate = rate; }
+  double getCommission() { return commission; }
+  void setHourlyRate(double rate) { hourlyRate = rate; }
   void setSalary(double rate) { salary = rate; }
   void setEmployeeName(String name) { employeeName = name; }
   void setHoursWorked(double hours) { hoursWorked = hours; }
+  void setCommission(double commiss) { commission = commiss; }
+  double calcTotalHourlyPay(double hours, double rate)
+  {
+    if (hours < 40)
+    {
+      totalPay = hours * rate;
+    }
+    else if (hours > 40)
+    {
+      double overtimeHours = hours - 40;
+      double overtimePay = rate * 2;
+      totalPay = (hours * rate) + (overtimeHours * overtimePay);
+    }
+    return totalPay;
+  }
 }
